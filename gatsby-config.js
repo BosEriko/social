69dfd77d2ab3a1664c2a627f629fa9a320dafdd3
BosEriko <bos.eriko@gmail.com>
2019-12-20 22:24:49 +0800

    update the gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Bos Eriko Social`,
+    description: `Yet another social network repository on the web (with Firebase for the data management)`,
+    author: `@BosEriko`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
